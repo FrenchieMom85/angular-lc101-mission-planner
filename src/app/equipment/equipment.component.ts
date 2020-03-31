17ce276cca96de7408a69df1ddbc7ba351f34ff2
amfzn503 <amfzn503@msn.com>
2020-03-30 20:32:16 -0500

    studio 2 bonus missions not complete

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipmentComponent implements OnInit {
 
+  equipment: string[] = ['Habitat dome', 'Drones', 'Food containers', 'Oxygen tanks'];
+  equipmentBeingEdited: string = null;
+
   constructor() { }
 
   ngOnInit() {
   }
-
+  add(equpmentName: string) {
+    this.equipment.push(equpmentName);
+  }
+  remove(item: string) {
+    let index = this.equipment.indexOf(item);
+    this.equipment.splice(index, 1);
+  }
+  edit(item: string) {
+    this.equipmentBeingEdited = item;
+  }
+  save(newItem: string) {
+    // item['newItem'] = item;
+    this.equipmentBeingEdited = newItem;
+  }
+ 
 }
--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperimentsComponent implements OnInit {
 
+  experiments: string [] = ["Mars soil sample", "Plant growth in habitat", "Human bone density"]
+  experimentBeingEdited: string = null;
+
   constructor() { }
 
   ngOnInit() {
   }
-
+  add(experimentName: string) {
+    this.experiments.push(experimentName);
+  }
+  remove(test: string) {
+    let index = this.experiments.indexOf(test);
+    this.experiments.splice(index, 1);
+  }
+  edit(test: string) {
+    this.experimentBeingEdited = test;
+  }
+  save(test: string) {
+    test['newTest'] = test;
+    this.experimentBeingEdited = null;
+  }
 }
